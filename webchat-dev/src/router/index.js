50b9061d4ce321830fcddbdb81145839fb7d1b4f
hhp1614 <971056445@qq.com>
2018-01-08 23:46:07 +0800

    router was ok

--- webchat-dev/src/router/index.js
+++ webchat-dev/src/router/index.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
 import Start from '../components/Start.vue'
 import Index from '../components/Index.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -17,13 +16,19 @@ export default new Router({
       path: '/index',
       name: 'Index',
       component: Index,
-      // children: [
-      //   {
-      //     path: '/index/page1',
-      //     name: 'page1',
-      //     component: page1
-      //   }
-      // ]
     }
   ]
 })
+
+/**
+ * 路由拦截
+ */
+router.beforeEach((to, from, next) => {
+  if (to.path === '/index' && !sessionStorage.getItem('user')) {
+    next('/')
+  } else {
+    next()
+  }
+})
+
+export default router
